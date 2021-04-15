16fe442648235962deed47e1b6a330308085d8b7
Srilekha Jagarlamudi <srilekhajagarlamudi.ce@gmail.com>
2021-04-15 02:12:12 -0700

    first commit

--- demo/settings.py
+++ demo/settings.py
@@ -65,7 +65,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-import os
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
